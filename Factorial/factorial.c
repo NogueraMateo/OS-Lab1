d64cb5955ef218605e78d296c544b4bc7c89e6bc
Mateo Noguera Pinto <mateo.edu.co@gmail.com>
2024-08-26 16:42:48 -0500

    Factorial and Leap year solved

---
+++ Factorial/factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculate_factorial(int n) {
+    if ( (n == 0) || (n == 1)) return 1;
+
+    return n * calculate_factorial(n-1);
+}
+
+int main() {
+    int number;
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("The factorial of negative numbers doesn't exist\n");
+        return 1;
+    }
+        
+    printf("The factorial of the number %d is: %d\n", number, calculate_factorial(number));
+    
+    return 0;
+}
---
+++ LeapYear/leap_year.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Function to check if a year is a leap year
+* @param year: the year to check
+ */
+int is_leap_year(int year) {
+    // If the year is divisible by 400, then it's divisible by 100 and 4
+    if ((year % 400) == 0) {
+        return 1;
+    }
+
+    // If the year is divisible by 100, but not by 400, then it's not a leap year
+    if ((year % 100) == 0) {
+        return 0;
+    }
+
+    // If the year is only divisible by 4, then it's a leap year
+    if ((year % 4) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if (is_leap_year(year)) {
+        printf("The year %d is a leap year\n", year);
+    } else {
+        printf("The year %d is not a leap year\n", year);
+    }
+
+    return 0;
+}
